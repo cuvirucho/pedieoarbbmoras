f9f5198a9535d4c1c435cd8bdd1a276545a7d3e2
cuvirucho <dani.leonardo@hotmail.com>
2025-08-21 15:14:35 -0600

    Primer copreco

--- src/fitbase/Firebase.js
+++ src/fitbase/Firebase.js
@@ -8,15 +8,14 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-SvcKVRPhndA5Kml7x9qI6fC0nLP3O44",
-  authDomain: "morasnoelinar.firebaseapp.com",
-  projectId: "morasnoelinar",
-  storageBucket: "morasnoelinar.firebasestorage.app",
-  messagingSenderId: "256331187104",
-  appId: "1:256331187104:web:2274870527199d79cb9ac5",
-  measurementId: "G-JZME4KJXYM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
